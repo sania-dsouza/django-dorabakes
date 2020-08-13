5168fd86095d2463ccfbc6cf353041f610b69428
Sania <sania.d@myplanet.com>
2020-08-12 22:59:09 -0400

    Added another savory recipe and fixed cards alignment

--- doraBakes/doraBakes/settings.py
+++ doraBakes/doraBakes/settings.py
@@ -120,10 +120,9 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
-STATIC_ROOT = os.path.join(BASE_DIR, 'doraBakes/bakery/static')
+
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (
   os.path.join(BASE_DIR, 'bakery/static/'),
-    os.path.join(BASE_DIR, 'bakery/'),
 )
 CSRF_COOKIE_DOMAIN= None
